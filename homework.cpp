a4ff0c2d6a4e34d2f01d33ebfb70ac1572ad8e22
kimi1273698276342691734617364e <kimthai002@gmail.com>
2024-12-19 21:48:52 +0000

    prob3

--- homework.cpp
+++ homework.cpp
@@ -6,14 +6,43 @@ int main()
     // Complete the homework problems here
     
     // Problem 1
-    
+std::cout << "Pick a number from 1 to 100.";
+std::cin << number;
+
+while (number < 1 || number > 100)
+{
+    std:: cout << "Invalid. Please pick another number.";
+    std:: cin << number
+}
+
+std::cout << "Thank you for your input :)";
     
     // Problem 2
-    
+int guesses = 1;
+std::string favColor = "red";
+std::string userGuess;
+
+std::cout << "Guess my favorite color...";
+std::cin << userGuess;
+
+while (userGuess != favColor)
+{
+    std::cout << "Incorrect...Try again :) ";
+    std::cin << userGuess;
+    guesses = guesses + 1;
+}
+std::cout << "Correct! That took you " << guesses << " attempt(s)." << std::endl;
     
     // Problem 3
-    
-    
+int sum = 0;
+int userNum;
+
+std::cout << "How many numbers do you want to add? ";
+std::cin << userNum;
+
+for ()
+
+
     
     return 0;
 }
